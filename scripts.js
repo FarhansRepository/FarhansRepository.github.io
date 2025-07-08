4812fc65b9307049dc5b30e852dd26b675262113
Farhan Mohamamd <134121258+FarhansRepository@users.noreply.github.com>
2025-07-08 19:12:38 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,19 @@
+<script>
+  const text = document.getElementById("text");
+  const original = text.innerText;
+  const chars = "!@#$%^&*()_+1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  function glitch() {
+    let newText = "";
+    for(let i = 0; i < original.length; i++) {
+      if(Math.random() > 0.7) {
+        newText += chars[Math.floor(Math.random() * chars.length)];
+      } else {
+        newText += original[i];
+      }
+    }
+    text.innerText = newText;
+  }
+  
+  setInterval(glitch, 100); // Speed in ms
+</script>
