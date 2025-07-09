8dd590230845f1afb7189f91c3bbaa213c762cc6
Farhan Mohamamd <134121258+FarhansRepository@users.noreply.github.com>
2025-07-09 16:34:43 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const text = document.getElementById("text");
     if (!text) return;
 
-    /*const original = text.textContent;
+    const original = text.textContent;
     /*const chars = "!@#$%^&*()_+1234567890";*/
     const originalSegments = ["Fa", "r", "ha", "n", "Mo", "ha", "mm", "ad"];
     const chars = ["ফা", "র", "হা", "ন", "মো", "হা", "ম্ম", "দ"]
