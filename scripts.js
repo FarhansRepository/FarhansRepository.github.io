4530f860248712e9d74008ac71ed06a751e1168b
Farhan Mohamamd <134121258+FarhansRepository@users.noreply.github.com>
2025-07-10 19:18:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", () => {
     const text = document.getElementById("text");
     if (!text) return;
@@ -25,4 +26,21 @@ document.addEventListener("DOMContentLoaded", () => {
     
     setInterval(glitch, 1000); //run glitch every 1000 seconds
 });
+*/
+document.addEventListener("DOMContentLoaded", () => {
+    const text = document.getElementById("text");
+    if (!text) return;
 
+    const original = text.textContent;
+    /*const chars = "!@#$%^&*()_+1234567890";*/
+    const chars = "01_#";
+    function glitch() {
+        const glitched = original.split('').map(char => 
+            Math.random() > 0.7 ? chars[Math.floor(Math.random() * chars.length)] : char
+        ).join('');
+        text.textContent = glitched;
+        setTimeout(() => text.textContent = original, 1000);
+    }
+
+    setInterval(glitch, 1000); // glitch every second
+});
