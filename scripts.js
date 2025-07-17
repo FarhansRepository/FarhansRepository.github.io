2672349aa22a3c81b893418a90c920a9ffd968d7
Farhan Mohamamd <134121258+FarhansRepository@users.noreply.github.com>
2025-07-16 20:00:23 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const element = document.getElementById("typewriter");
-  const words = ["FARHAN::SITE", "HE/HIM/সে", "বাঙ্গালী-AMERICAN", "مسلم ملحد", "COMP-SCI GRAD", "LEARNING & BUILDING"];
+  const words = ["FARHAN::SITE", "HE/HIM/সে", "বাঙ্গালী-AMERICAN", "مسلم ملحد", "COMP-SCI GRAD", "JACK OF ALL TRADES"];
   let wordIndex = 0;
   let charIndex = 0;
   let isDeleting = false;
